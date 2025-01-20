16e290457bc8676deb52a4326b26ba8dcfa4fb87
Kraken <krakeen99@gmail.com>
2025-01-20 10:10:08 +0200

    Delete src directory

--- src/main/java/ee/ivkhk/NPTV23Store/interfaces/AppService.java
+++
@@ -1,8 +0,0 @@
-package ee.ivkhk.NPTV23Store.interfaces;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface AppService<T, ID> extends JpaRepository<T, ID> {
-}
